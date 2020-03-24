0adae54778ef16307e6d071b1715b1a9f8a490b3
s18705 <18705@pjwstk.edu.pl>
2020-03-24 15:08:46 +0100

    Dodaj pliki projektów.

---
+++ APBDcw3(2)/Controllers/WeatherForecastController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace APBDcw3_2_.Controllers
+{
+    [ApiController]
+    [Route("api/student")]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
+        { 
+            if (id == 1)
+            {
+                return Ok("Kowalski");
+            }else if (id == 2)
+            {
+                return Ok("Malewski");
+            }
+            return NotFound("Nie znaleziono studenta");
+        }
+    }
+}
