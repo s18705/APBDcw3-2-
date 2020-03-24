f3010130dcdf79a10995d1668ee025fd4b62c697
s18705 <18705@pjwstk.edu.pl>
2020-03-24 22:32:33 +0100

    zadanie 7

---
+++ APBDcw3(2)/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APBDcw3_2_.Models
+{
+    public class Studen
+    {
+        public int IdStudent { get; set; }
+        
+        public string FirstName { get; set; }
+
+        public string LasName { get; set; }
+
+        public string IndexNumber { get; set; }
+    }
+}
